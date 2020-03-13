8d2ea19945c8292d6df8dcf789c2eb53b8b5f60a
Jen E <jenevans3ten@gmail.com>
2020-03-13 12:57:52 -0400

    morning workshop additions:

---
+++ frontend/src/js/components/Todos.js
@@ -0,0 +1,13 @@
+export default function Todos(todos){
+    return `
+        <ul>
+            ${todos.map(todo => {
+                return `
+                <li>
+                    <h3>${todo}</h3>
+                </li>
+                `  
+            })}          
+        </ul>
+    `
+}
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,12 +1,17 @@
 // List of imports
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Todos from './components/Todos';
+import apiActions from './api/apiActions';
 
 pageBuild();
 
 function pageBuild(){
     header();
     footer();
+    navHome();
+    navTodo();
 }
 
 function header() {
@@ -17,4 +22,19 @@ function header() {
 function footer(){
     const footer = document.querySelector('#footer');
     footer.innerHTML = Footer();
+}
+
+function navHome(){
+    const homeButton = document.querySelector('.nav__home');
+    homeButton.addEventListener("click", function() {
+        document.querySelector("#app").innerHTML = Home();
+    });
+}
+
+function navTodo(){
+    const todosButton = document.querySelector(".nav__todos");
+    todosButton.addEventListener("click", function() {
+        alert('you clicked the todo button');
+      document.querySelector('#app').innerHTML = Todos(todos);
+      });
 }

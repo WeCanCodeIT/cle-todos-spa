2d702f07ac301f781e7e3d074674b6340080a4f2
Jen E <jenevans3ten@gmail.com>
2020-03-17 17:37:19 -0400

    cleaned up startup and added commments explaining each service and configuration

--- backend/todos/Startup.cs
+++ backend/todos/Startup.cs
@@ -17,25 +17,34 @@ namespace todos
 {
     public class Startup
     {
+        // used for Web APIs, when IConfiguration is injected into the 
+        // Startup constructor
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        // .net 3.1 for cors issue
+        // used for asp.net 3.1 cors issue
         readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+        // used for Web APIs
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddMvc();
+            // used for Web APIs to add MVC Controllers, but not Razor pages and Views
+            // Adds services for Authorization, Validation, formattors, CORS, etc.
+            services.AddControllers(); 
+
+            // Not needed for Web APIs as it adds both Views and Razor Pages,
+            // which are unnecessary for our Web API
+            // services.AddMvc();
+
             services.AddDbContext<TodoContext>();
             services.AddScoped<IRepository<Todo>, TodoRepository>();
 
-            // .net 3.1 for cors issue
+            // used for asp.net 3.1 cors issue
             services.AddCors(options =>
             {
                 options.AddPolicy(MyAllowSpecificOrigins,
@@ -52,22 +61,32 @@ namespace todos
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // used for any asp.net empty template
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            // .net 3.1 cors issue
+            // used for asp.net 3.1 cors issue
             app.UseCors(MyAllowSpecificOrigins);
 
+            // used for Web APIs, to ensure requests are made over secure domain
             app.UseHttpsRedirection();
 
+            // used for any asp.net empty template, it looks at incoming request
+            // and decides which endpoint should execute
             app.UseRouting();
 
+            // used for Web APIs
             app.UseAuthorization();
 
+            // used for any asp.net empty template to configure and execute endpoints, 
+            // endpoint routing is the preferred approach for Core versions 3.0 +
             app.UseEndpoints(endpoints =>
             {
+                // this Endpoint implementation used for Web APIs
+                // It only maps controllers that have routing attributes, 
+                // for example [Route("api/[controller]")] 
                 endpoints.MapControllers();
             });
         }
